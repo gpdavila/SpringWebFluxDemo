caa54a659a87bed0b6c57b448ead1b88d883fd96
Gustavo de Oliveira <gustavooliveira@sicredi.com.br>
2024-04-22 18:02:14 -0300

    feat: tratamento de excecao client adicionado

--- src/main/java/io/sicredi/client/estoque/EstoqueClient.java
+++ src/main/java/io/sicredi/client/estoque/EstoqueClient.java
@@ -1,8 +1,10 @@
 package io.sicredi.client.estoque;
 
 import io.sicredi.domain.estoque.Estoque;
+import io.sicredi.error.ExceptionWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -24,6 +26,10 @@ public class EstoqueClient {
         .get()
         .uri("/v3/067ab719-5f04-4c3a-90a5-bb1996b3cda1")
         .retrieve()
+        .onStatus(HttpStatusCode::is4xxClientError,
+            ExceptionWrapper::toBusinessException)
+        .onStatus(HttpStatusCode::is5xxServerError,
+            ExceptionWrapper::toTechnicalException)
         .bodyToMono(Estoque.class);
   }
 }
